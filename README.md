82ff45f00e217cd4401aebcd1745c1fcf2c75077
Gaurav Sarma <gauravsarma1992@gmail.com>
2023-07-14 22:48:26 +0530

    Setting up basic files

---
+++ README.md
@@ -0,0 +1,2 @@
+# PyPool
+A python module to easily spawn parallel python processes working on a redis queue
--- pypool/event_store.py
+++ pypool/event_store.py
@@ -1,3 +1,10 @@
+
+
+class Event:
+    def __init__(self):
+        pass
+
+
 class EventStore:
     def __init__(self):
         pass
